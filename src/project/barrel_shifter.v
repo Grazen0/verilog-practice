d94217d08d4b7b5a953610889119f8c042fa4d8b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-09-29 18:20:34 -0500

    feat: add barrel_shifter

---
+++ src/project/barrel_shifter.v
@@ -0,0 +1,29 @@
+module barrel_shifter #(
+    parameter WIDTH = 32
+) (
+    input wire [WIDTH-1:0] in,
+    input wire [$clog2(WIDTH)-1:0] shift,
+    input wire dir,
+    output reg [WIDTH-1:0] out
+);
+  integer i;
+  reg [WIDTH-1:0] stage[0:$clog2(WIDTH)];
+
+  always @(*) begin
+    stage[0] = in;
+
+    for (i = 0; i < $clog2(WIDTH); i = i + 1) begin
+      if (shift[i]) begin
+        if (dir) begin
+          stage[i+1] = stage[i] >> (1 << i);
+        end else begin
+          stage[i+1] = stage[i] << (1 << i);
+        end
+      end else begin
+        stage[i+1] = stage[i];
+      end
+    end
+
+    out = stage[$clog2(WIDTH)];
+  end
+endmodule
---
+++ tb/project/barrel_shifter_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module barrel_shifter_tb ();
+  localparam WIDTH = 8;
+
+  reg [WIDTH-1:0] in;
+  reg [$clog2(WIDTH)-1:0] shift;
+  wire [WIDTH-1:0] out;
+
+  barrel_shifter shifter (
+      .in(in),
+      .shift(shift),
+      .out(out)
+  );
+
+  initial begin
+    $dumpvars(0, barrel_shifter_tb);
+
+    in = 8'b0101_0110;
+    shift = 0;
+
+    repeat (10) begin
+      #1 shift = shift + 1;
+    end
+
+    $finish();
+  end
+endmodule
