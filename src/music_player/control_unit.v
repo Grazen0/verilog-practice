7b4a3ddfa8148ddca2f9e8713a2e8c478d4e83fd
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-01 22:14:42 -0500

    style: format files

--- src/music_player/control_unit.v
+++ src/music_player/control_unit.v
@@ -12,64 +12,64 @@ module control_unit #(
     output wire [$clog2(ROM_SIZE)-1:0] rom_addr,
     output wire [15:0] speaker_divider
 );
-  localparam BEAT_PERIOD = (60 * CLK_FREQ) / (TEMPO * BEAT_SCALE);
+    localparam BEAT_PERIOD = (60 * CLK_FREQ) / (TEMPO * BEAT_SCALE);
 
-  reg init;
-  reg inc_nc;
-  reg [$clog2(ROM_SIZE)-1:0] note_counter;
-  reg [ROM_WIDTH-1:0] note_buf;
+    reg init;
+    reg inc_nc;
+    reg [$clog2(ROM_SIZE)-1:0] note_counter;
+    reg [ROM_WIDTH-1:0] note_buf;
 
-  wire [NOTE_DURATION_WIDTH-1:0] note_duration = rom_data[8+:NOTE_DURATION_WIDTH];
-  wire [7:0] note_index = note_buf[7:0];
+    wire [NOTE_DURATION_WIDTH-1:0] note_duration = rom_data[8+:NOTE_DURATION_WIDTH];
+    wire [7:0] note_index = note_buf[7:0];
 
-  wire beat_done, note_done;
+    wire beat_done, note_done;
 
-  auto_reload_timer beat_timer (
-      .clk(clk),
-      .rst_n(rst_n),
-      .load_value(BEAT_PERIOD),
-      .enable(init),
-      .done(beat_done)
-  );
+    auto_reload_timer beat_timer (
+        .clk(clk),
+        .rst_n(rst_n),
+        .load_value(BEAT_PERIOD),
+        .enable(init),
+        .done(beat_done)
+    );
 
-  auto_reload_timer #(
-      .WIDTH(NOTE_DURATION_WIDTH)
-  ) note_timer (
-      .clk(clk),
-      .rst_n(rst_n),
-      .load_value(note_duration),
-      .enable(init & beat_done),
-      .done(note_done)
-  );
+    auto_reload_timer #(
+        .WIDTH(NOTE_DURATION_WIDTH)
+    ) note_timer (
+        .clk(clk),
+        .rst_n(rst_n),
+        .load_value(note_duration),
+        .enable(init & beat_done),
+        .done(note_done)
+    );
 
-  note_data #(
-      .CLK_FREQ(CLK_FREQ)
-  ) nd (
-      .note_index(note_index),
-      .divider(speaker_divider)
-  );
+    note_data #(
+        .CLK_FREQ(CLK_FREQ)
+    ) nd (
+        .note_index(note_index),
+        .divider(speaker_divider)
+    );
 
-  always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      init <= 0;
-      inc_nc <= 0;
-      note_counter <= 0;
-      note_buf <= 0;
-    end else if (!init) begin
-      // Waits for ROM data to arrive
-      init <= 1;
-    end else if (note_done) begin
-      note_buf <= rom_data;
-      inc_nc   <= 1;
-    end else if (inc_nc) begin
-      if (note_counter == ROM_SIZE - 1) begin
-        note_counter <= 0;
-      end else begin
-        note_counter <= note_counter + 1;
-      end
-      inc_nc <= 0;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            init <= 0;
+            inc_nc <= 0;
+            note_counter <= 0;
+            note_buf <= 0;
+        end else if (!init) begin
+            // Waits for ROM data to arrive
+            init <= 1;
+        end else if (note_done) begin
+            note_buf <= rom_data;
+            inc_nc   <= 1;
+        end else if (inc_nc) begin
+            if (note_counter == ROM_SIZE - 1) begin
+                note_counter <= 0;
+            end else begin
+                note_counter <= note_counter + 1;
+            end
+            inc_nc <= 0;
+        end
     end
-  end
 
-  assign rom_addr = note_counter;
+    assign rom_addr = note_counter;
 endmodule
--- tb/music_player/control_unit_tb.v
+++ tb/music_player/control_unit_tb.v
@@ -3,46 +3,46 @@
 `include "music_player/notes.vh"
 
 module control_unit_tb ();
-  localparam CLK_FREQ = 100000;
-  localparam ROM_WIDTH = 16;
-  localparam ROM_SIZE = 4;
+    localparam CLK_FREQ = 100000;
+    localparam ROM_WIDTH = 16;
+    localparam ROM_SIZE = 4;
 
-  reg clk, rst_n;
-  reg [ROM_WIDTH-1:0] rom[ROM_SIZE];
-  wire [ROM_WIDTH-1:0] rom_data;
-  wire [$clog2(ROM_SIZE)-1:0] rom_addr;
-  wire [15:0] speaker_divider;
+    reg clk, rst_n;
+    reg [ROM_WIDTH-1:0] rom[ROM_SIZE];
+    wire [ROM_WIDTH-1:0] rom_data;
+    wire [$clog2(ROM_SIZE)-1:0] rom_addr;
+    wire [15:0] speaker_divider;
 
-  always #1 clk = ~clk;
+    always #1 clk = ~clk;
 
-  assign rom_data = rom[rom_addr];
+    assign rom_data = rom[rom_addr];
 
-  control_unit #(
-      .CLK_FREQ (CLK_FREQ),
-      .ROM_SIZE (ROM_SIZE),
-      .ROM_WIDTH(ROM_WIDTH)
-  ) ctrl (
-      .clk(clk),
-      .rst_n(rst_n),
-      .rom_addr(rom_addr),
-      .rom_data(rom_data),
-      .speaker_divider(speaker_divider)
-  );
+    control_unit #(
+        .CLK_FREQ (CLK_FREQ),
+        .ROM_SIZE (ROM_SIZE),
+        .ROM_WIDTH(ROM_WIDTH)
+    ) ctrl (
+        .clk(clk),
+        .rst_n(rst_n),
+        .rom_addr(rom_addr),
+        .rom_data(rom_data),
+        .speaker_divider(speaker_divider)
+    );
 
-  localparam SIMULATION_SECONDS = 5;
+    localparam SIMULATION_SECONDS = 5;
 
-  initial begin
-    $dumpvars(0, control_unit_tb);
+    initial begin
+        $dumpvars(0, control_unit_tb);
 
-    rom[0] = `NOTE(`C3, 8);
-    rom[1] = `NOTE(`D3, 16);
-    rom[2] = `NOTE(`SILENCE, 4);
-    rom[3] = `NOTE(`F3, 4);
+        rom[0] = `NOTE(`C3, 8);
+        rom[1] = `NOTE(`D3, 16);
+        rom[2] = `NOTE(`SILENCE, 4);
+        rom[3] = `NOTE(`F3, 4);
 
-    clk = 1;
-    rst_n = 0;
-    #1 rst_n = 1;
+        clk = 1;
+        rst_n = 0;
+        #1 rst_n = 1;
 
-    #(2 * CLK_FREQ * SIMULATION_SECONDS) $finish();
-  end
+        #(2 * CLK_FREQ * SIMULATION_SECONDS) $finish();
+    end
 endmodule
